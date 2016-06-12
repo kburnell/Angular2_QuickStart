bdae95486e77301a197794bb70b3ad12b6fa0119
Keith Burnell <keith@kburnell.com>
2016-06-12 12:51:32 -0500

    Addded notes

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+//Angular's browser bootstrap function
+//Boostrapping isn't in @angular/core because there isn't a single to boostrap an app.
+//could be browser or mobile device which requires a different kind of bootstrapping function which would be imported from a different library.
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+//Application's root component
+import { AppComponent } from './app.component';
+
+//Bootstrap application
+bootstrap(AppComponent);
